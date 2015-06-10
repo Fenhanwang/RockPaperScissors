b0dbf067f82a0c70ba61267abd424aad7c6cdac0
Fenhanwang <wfenhan@gmail.com>
2015-06-10 00:00:31 -0400

    Complete the game

---
+++ app/controllers/playgames_controller.rb
@@ -0,0 +1,13 @@
+class PlaygamesController < ApplicationController
+	def index
+		result = ["rock", "scissors", "paper"].sample
+		decide = { "rock"=>"scissors", "scissors"=>"paper","paper"=>"rock" }
+		@flag = (decide[params[:sign]] == result) ? 1 : 0
+		@tie = params[:sign].to_s == result ? "tie" : "nottie"
+		gon.result = result
+		respond_to do |format|
+			format.html { render :index }
+			format.js
+		end
+	end
+end
